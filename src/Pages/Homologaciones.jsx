7e49c3cd5afa6685930f23c88ca3f0096dd98432
Juan Pablo Bautista <juan pablo bautista>
2024-01-14 19:50:29 -0500

    Creado la pagina de Hmologaciones y ya e ajustado el navbar

--- src/Pages/Homologaciones.jsx
+++ src/Pages/Homologaciones.jsx
@@ -1,14 +1,30 @@
 export default function Homologacion() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#fcf2e8] dark:bg-gray-900">
       <div className="w-full max-w-4xl mx-auto flex flex-row justify-between gap-4">
         <div className="flex-1 flex flex-col items-center justify-center p-8 bg-white border border-gray-200 rounded-md shadow-md dark:bg-gray-800 dark:border-gray-700 m-4 w-[800px] h-[600px]">
           <h2 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
             Upload HTML File
           </h2>
+
           <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
             Drag drop some files here, or click to select files
           </p>
+
+          {/* Modificado el botón para cargar manualmente */}
+          <label
+            htmlFor="file-upload"
+            className="mt-4 cursor-pointer bg-indigo-500 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-indigo-600"
+          >
+            Upload File Manually
+          </label>
+          <input
+            type="file"
+            id="file-upload"
+            accept=".html"
+            className="hidden" // Oculta el input de tipo "file"
+          />
+
           <div className="mt-8 relative">
             <label className="sr-only" htmlFor="file-upload">
               Upload File
