e4732368d9b1a9b1175b5ff2664fa24ee8dd050e
Juan Pablo Bautista <juan pablo bautista>
2024-01-16 18:28:33 -0500

    Todo el frontend visual esta ya implementado

--- src/Pages/Usuarios/Usuarios.jsx
+++ src/Pages/Usuarios/Usuarios.jsx
@@ -7,14 +7,16 @@ export default function Usuarios() {
       <Navbar></Navbar>
       <section className="empleados" id="empleados">
         <div className="tituloEmpleados">
-          <span> Empleados </span>
-          <h2>Información de nuestros empleados </h2>
+          <span> Estudiantes </span>
+          <h2>
+            Información de los estudiantes que han solicitado Homologación{" "}
+          </h2>
         </div>
 
         <div className="botones">
           <button className="boton">
             <a href="/dashboard/Forms/employe" className="navbar__menu--link">
-              Agregar Empleado
+              Agregar Estudiantes
             </a>
           </button>
           <button className="boton">Descargar Reporte</button>
@@ -24,84 +26,70 @@ export default function Usuarios() {
           <div className="cajaEmpleado">
             <div className="imgEmpleado">
               <img
-                src="/img/rev2.jpg"
+                src="/src/assets/logoipsum-214.svg"
                 width={800}
                 height={400}
-                alt="Empleado 2"
+                alt="Foto"
               />
             </div>
             <h3>Pepe Perez</h3>
-            <p>Código: EMP002</p>
-            <p>Cargo: Diseñador</p>
-            <p>Sucursal: Sucursal B</p>
-            <p>Salario: $45,000</p>
+            <p> Código: EMP002</p>
+            <p>Proyecto curricular: Tec. sistematizacion </p>
+            <p>Sede: Tecnologica</p>
             <p>Fecha de Ingreso: 02/01/2022</p>
             <div className="botonCard">
-              <button className="btnc">Eliminar Empleado</button>
-              <button className="btnc">Modificar Empleado</button>
+              <button className="btnc">Eliminar Homologacion</button>
+              <button className="btnc">Ver Homologacion</button>
             </div>
           </div>
 
           <div className="cajaEmpleado">
             <div className="imgEmpleado">
               <img
-                src="/img/rev3.jpg"
+                src="/src/assets/react.svg"
                 width={800}
                 height={400}
                 alt="Empleado 3"
               />
             </div>
-            <h3>Angelica Martinez</h3>
-            <p>Código: EMP003</p>
-            <p>Cargo: Gerente</p>
-            <p>Sucursal: Sucursal C</p>
-            <p>Salario: $70,000</p>
-            <p>Fecha de Ingreso: 03/01/2022</p>
+            <h3>Juliana Martinez</h3>
+            <p>Código: EMP002</p>
+            <p>Proyecto curricular: Tec. Industrial </p>
+            <p>Sede: Tecnologica</p>
+            <p>Fecha de Ingreso: 02/01/2022</p>
             <div className="botonCard">
-              <button className="btnc">Eliminar Empleado</button>
-              <button className="btnc">Modificar Empleado</button>
+              <button className="btnc">Eliminar Homologacion</button>
+              <button className="btnc">Ver Homologacion</button>
             </div>
           </div>
 
           <div className="cajaEmpleado">
             <div className="imgEmpleado">
-              <img
-                src="/img/woman.png"
-                width={800}
-                height={400}
-                alt="Empleado 3"
-              />
+              <img src="/img/woman.png" width={800} height={400} alt="Foto" />
             </div>
-            <h3>Angelica Martinez</h3>
-            <p>Código: EMP003</p>
-            <p>Cargo: Gerente</p>
-            <p>Sucursal: Sucursal C</p>
-            <p>Salario: $70,000</p>
-            <p>Fecha de Ingreso: 03/01/2022</p>
+            <h3>Maria Alfarez</h3>
+            <p>Código: EMP002</p>
+            <p>Proyecto curricular: Tec. Civil </p>
+            <p>Sede: Tecnologica</p>
+            <p>Fecha de Ingreso: 02/01/2022</p>
             <div className="botonCard">
-              <button className="btnc">Eliminar Empleado</button>
-              <button className="btnc">Modificar Empleado</button>
+              <button className="btnc">Eliminar Homologacion</button>
+              <button className="btnc">Ver Homologacion</button>
             </div>
           </div>
 
           <div className="cajaEmpleado">
             <div className="imgEmpleado">
-              <img
-                src="/img/man.png"
-                width={800}
-                height={400}
-                alt="Empleado 3"
-              />
+              <img src="/img/man.png" width={800} height={400} alt="Foto" />
             </div>
-            <h3>Angelica Martinez</h3>
-            <p>Código: EMP003</p>
-            <p>Cargo: Gerente</p>
-            <p>Sucursal: Sucursal C</p>
-            <p>Salario: $70,000</p>
-            <p>Fecha de Ingreso: 03/01/2022</p>
+            <h3>Juan Pedro Perez</h3>
+            <p>Código: EMP002</p>
+            <p>Proyecto curricular: Tec. Mecanica </p>
+            <p>Sede: Tecnologica</p>
+            <p>Fecha de Ingreso: 02/01/2022</p>
             <div className="botonCard">
-              <button className="btnc">Eliminar Empleado</button>
-              <button className="btnc">Modificar Empleado</button>
+              <button className="btnc">Eliminar Homologacion</button>
+              <button className="btnc">Ver Homologacion</button>
             </div>
           </div>
 
@@ -111,7 +99,7 @@ export default function Usuarios() {
                 src="/img/iconUserF.png"
                 width={800}
                 height={400}
-                alt="Empleado 3"
+                alt="Foto"
               />
             </div>
             <h3>Angelica Martinez</h3>
@@ -121,8 +109,8 @@ export default function Usuarios() {
             <p>Salario: $70,000</p>
             <p>Fecha de Ingreso: 03/01/2022</p>
             <div className="botonCard">
-              <button className="btnc">Eliminar Empleado</button>
-              <button className="btnc">Modificar Empleado</button>
+              <button className="btnc">Eliminar Homologacion</button>
+              <button className="btnc">Ver Homologacion</button>
             </div>
           </div>
         </div>
