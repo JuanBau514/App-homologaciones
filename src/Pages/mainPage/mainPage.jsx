98900a0bfaac94f3c0079615c7eaef387b8170b9
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2024-01-27 09:29:27 -0500

    links arreglados

--- src/Pages/mainPage/mainPage.jsx
+++ src/Pages/mainPage/mainPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Navbar from "../../Components/Navbar";
 import "./main.css";
 
@@ -23,13 +24,17 @@ export default function Mainpage() {
                 className="inline-flex h-10 items-center justify-center rounded-md bg-[#8B0000] px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#fcd116] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#0b162a] disabled:pointer-events-none disabled:opacity-50 dark:bg-[#fcd116] dark:text-gray-900 dark:hover:bg-[#ffcd00] dark:focus-visible:ring-[#0b162a]"
                 href="#"
               >
-                Comenzar
+                <Link to="/Homologaciones">
+                  <span className="mr-2">Comenzar</span>
+                </Link>
               </a>
               <a
                 className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
                 href="#"
               >
-                Acerca de
+                <Link to="https://github.com/JuanBau514/App-homologaciones">
+                  Repositorio
+                </Link>
               </a>
             </div>
           </div>
